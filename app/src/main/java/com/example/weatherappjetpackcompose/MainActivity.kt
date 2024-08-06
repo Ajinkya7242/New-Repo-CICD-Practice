bfb45ad96a103bcb6c88a677bc1f867d3c02a46c
ASUS <waghmodeajinkya80@gmail.com>
2024-08-06 12:33:10 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/weatherappjetpackcompose/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.weatherappjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatherappjetpackcompose.ui.theme.WeatherAppJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherAppJetpackComposeTheme {
+                Surface {
+                    MainScreen()
+                }
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainScreen() {
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xff313968),
+                        Color(0xff525d93)
+                    )
+                )
+            )
+    ) {
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherAppJetpackComposeTheme {
+        Greeting("Android")
+    }
+}
